fd52879df961bd6ac4c0f5ed57ba50e605573168
Ben-he321 <benhe321@gmail.com>
2026-06-28 05:39:43 +0200

    Add trading journal page

---
+++ pages/4_📒_交易日志.py
@@ -0,0 +1,10 @@
+"""交易日志页面占位。"""
+
+import streamlit as st
+
+
+st.set_page_config(page_title="交易日志", page_icon="📒", layout="centered")
+
+st.title("交易日志")
+st.info("将显示：历史推荐、实际结果、模拟盘战绩")
+st.warning("🚧 建设中（M1+）")
